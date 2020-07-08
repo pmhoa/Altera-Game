79513c3a5e01e89819972bc250bf5db4d76da467
MestreCale <67955995+MestreCale@users.noreply.github.com>
2020-07-08 14:25:46 +0300

    Created Stat Classes
    Created WeaponStat
    Created ArmourStat
    Created UnitStat
    and an interface IDamagble

---
+++ Assets/Scripts/Stats/UnitStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats
+{
+
+    private float _health;
+    private float _aim;
+    private float _dodge;
+
+    public float Health
+    {
+        get => _health;
+        set => _health = value;
+    }
+    public float Aim
+    {
+        get => _aim;
+        set => _aim = value;
+    }
+    public float Dodge
+    {
+        get => _dodge;
+        set => _dodge = value;
+    }
+
+  
+
+}
---
+++ Assets/Scripts/Stats/WeaponStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats 
+{
+
+    private float _accuracy;
+    private float _fireRate;
+    private float _damage;
+    private float _range;
+
+    public float Accuracy
+    {
+        get => _accuracy;
+        set => _accuracy = value;
+    }
+    public float FireRate
+    {
+        get => _fireRate;
+        set => _fireRate = value;
+    }
+    public float Damage
+    {
+        get => _damage;
+        set => _damage = value;
+    }
+    public float Range
+    {
+        get => _range;
+        set => _fireRate = value;
+    }
+}
